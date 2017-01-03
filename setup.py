19954f6d9c6c06a2a82eebe951d99e08c0a62a42
Mehdi Cherti <mehdicherti@gmail.com>
2017-01-03 01:42:37 +0100

    switch to setuptools and add requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
+from __future__ import print_function
+import sys
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
 setup(name='convnetskeras',
       version='0.1',
-      description='Pre-trained convnets in Keras',
+      description='Pre-trained convnets in keras',
       author='Leonard Blier',
       author_email='leonard.blier@ens.fr',
-      packages=['convnetskeras'],
-      package_dir={'convnetskeras':'convnetskeras'},
-      package_data={'convnetskeras':["data/*"]},
-      long_description=open('README.md').read(),
-     )
-
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      )
